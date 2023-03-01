097fdd944761e3086bc2b35492bc99118db5bae8
mk <mkalugin21@gmail.com>
2023-03-01 20:19:37 +0400

    add componets header and cardlist

---
+++ src/components/CardList/CardList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function CardList() {
+    return (
+        <section className="section-card-list">
+            <h2 className="title-section">Все кроссовки</h2>
+            <ul className="card-list">
+                
+            </ul>
+        </section>
+    )
+}
+
+export default CardList;
