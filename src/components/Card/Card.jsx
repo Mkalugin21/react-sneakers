7c6f87ef182d89f8d8876ff74df838d67130c852
mk <mkalugin21@gmail.com>
2023-03-04 22:11:54 +0400

    add card layout

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./card.scss"
+
+function Card() {
+    return (
+        <li className="card">
+            <img className="card__image" src="./image/sneakers/sneakers.jpg" alt="" />
+            <p className="card__name">Мужские Кроссовки Nike Blazer Mid Suede</p>
+            <div className="card-price-wrapper">
+                <p className="price-title">Цена:</p>
+                <p className="price-value">12 999 руб.</p>
+                <button className="button-add-cart">
+                    <img className="card-add__image" src="./image/plus.svg" alt="" />
+                </button>
+            </div>
+        </li>
+    )
+};
+
+export default Card;
--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import Card from "../Card/Card";
+import styles from "./cardlist.scss"
 
 function CardList() {
     return (
         <section className="section-card-list">
             <h2 className="title-section">Все кроссовки</h2>
             <ul className="card-list">
-                
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
             </ul>
         </section>
     )
